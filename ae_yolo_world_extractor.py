5333e07c6125e5b01013603d4435353613ea5509
Arturs Elksnis <arturs@elksnis.co.uk>
2024-12-04 20:34:36 +0000

    Creating a class for yolo World extractor

--- ae_yolo_world_extractor.py
+++ ae_yolo_world_extractor.py
@@ -11,103 +11,97 @@ from mmyolo.registry import RUNNERS
 from torchvision.ops import nms
 
 
-class SemanticPathPlanner:
-    def __init__(self, scene_id, llm_type):
-        
+class YOLOWorldExtractor:
+    def __init__(self):
+        self.bounding_box_annotator = sv.BoxAnnotator()
+        self.label_annotator = sv.LabelAnnotator(text_position=sv.Position.CENTER)
 
-bounding_box_annotator = sv.BoxAnnotator()
-label_annotator = sv.LabelAnnotator(text_position=sv.Position.CENTER)
+        self.class_names = ("person, bicycle, car, motorcycle, airplane, bus, train, truck, boat, "
+                       "traffic light, fire hydrant, stop sign, parking meter, bench, bird, "
+                       "cat, dog, horse, sheep, cow, elephant, bear, zebra, giraffe, "
+                       "backpack, umbrella, handbag, tie, suitcase, frisbee, skis, snowboard, "
+                       "sports ball, kite, baseball bat, baseball glove, skateboard, "
+                       "surfboard, tennis racket, bottle, wine glass, cup, fork, knife, "
+                       "spoon, bowl, banana, apple, sandwich, orange, broccoli, carrot, "
+                       "hot dog, pizza, donut, cake, chair, couch, potted plant, bed, "
+                       "dining table, toilet, tv, laptop, mouse, remote, keyboard, "
+                       "cell phone, microwave, oven, toaster, sink, refrigerator, book, "
+                       "clock, vase, scissors, teddy bear, hair drier, toothbrush, plate, cloud, portrait, picture with clouds, tv table, glass table")
 
-class_names = ("person, bicycle, car, motorcycle, airplane, bus, train, truck, boat, "
-               "traffic light, fire hydrant, stop sign, parking meter, bench, bird, "
-               "cat, dog, horse, sheep, cow, elephant, bear, zebra, giraffe, "
-               "backpack, umbrella, handbag, tie, suitcase, frisbee, skis, snowboard, "
-               "sports ball, kite, baseball bat, baseball glove, skateboard, "
-               "surfboard, tennis racket, bottle, wine glass, cup, fork, knife, "
-               "spoon, bowl, banana, apple, sandwich, orange, broccoli, carrot, "
-               "hot dog, pizza, donut, cake, chair, couch, potted plant, bed, "
-               "dining table, toilet, tv, laptop, mouse, remote, keyboard, "
-               "cell phone, microwave, oven, toaster, sink, refrigerator, book, "
-               "clock, vase, scissors, teddy bear, hair drier, toothbrush, plate, cloud, portrait, picture with clouds, tv table, glass table")
+        # load config
+        self.cfg = Config.fromfile(
+            "./yolo_world_configs_and_weights/yolo_world_l_t2i_bn_2e-4_100e_4x8gpus_obj365v1_goldg_train_lvis_minival.py"
+        )
+        self.cfg.work_dir = "."
+        self.cfg.load_from = "./yolo_world_configs_and_weights/yolow-v8_l_clipv2_frozen_t2iv2_bn_o365_goldg_pretrain.pth"
+        self.runner = Runner.from_cfg(self.cfg)
+        self.runner.call_hook("before_run")
+        self.runner.load_or_resume()
+        self.pipeline = self.cfg.test_dataloader.dataset.pipeline
+        self.runner.pipeline = Compose(self.pipeline)
+        self.runner.model.eval()
 
-class_names2 = ("dog, eye, tongue, ear, leash")
+    def run_image(self,
+            runner,
+            input_image,
+            max_num_boxes=100,
+            score_thr=0.05,
+            nms_thr=0.5,
+            output_image="output.png",
+    ):
+        texts = [[t.strip()] for t in self.class_names.split(",")] + [[" "]]
+        data_info = runner.pipeline(dict(img_id=0, img_path=input_image,
+                                         texts=texts))
 
-def run_image(
-        runner,
-        input_image,
-        max_num_boxes=100,
-        score_thr=0.05,
-        nms_thr=0.5,
-        output_image="output.png",
-):
-    texts = [[t.strip()] for t in class_names.split(",")] + [[" "]]
-    data_info = runner.pipeline(dict(img_id=0, img_path=input_image,
-                                     texts=texts))
+        data_batch = dict(
+            inputs=data_info["inputs"].unsqueeze(0),
+            data_samples=[data_info["data_samples"]],
+        )
 
-    data_batch = dict(
-        inputs=data_info["inputs"].unsqueeze(0),
-        data_samples=[data_info["data_samples"]],
-    )
+        with autocast(enabled=False), torch.no_grad():
+            output = runner.model.test_step(data_batch)[0]
+            runner.model.class_names = texts
+            pred_instances = output.pred_instances
 
-    with autocast(enabled=False), torch.no_grad():
-        output = runner.model.test_step(data_batch)[0]
-        runner.model.class_names = texts
-        pred_instances = output.pred_instances
+        keep_idxs = nms(pred_instances.bboxes, pred_instances.scores, iou_threshold=nms_thr)
+        pred_instances = pred_instances[keep_idxs]
+        pred_instances = pred_instances[pred_instances.scores.float() > score_thr]
 
-    keep_idxs = nms(pred_instances.bboxes, pred_instances.scores, iou_threshold=nms_thr)
-    pred_instances = pred_instances[keep_idxs]
-    pred_instances = pred_instances[pred_instances.scores.float() > score_thr]
+        if len(pred_instances.scores) > max_num_boxes:
+            indices = pred_instances.scores.float().topk(max_num_boxes)[1]
+            pred_instances = pred_instances[indices]
+        output.pred_instances = pred_instances
 
-    if len(pred_instances.scores) > max_num_boxes:
-        indices = pred_instances.scores.float().topk(max_num_boxes)[1]
-        pred_instances = pred_instances[indices]
-    output.pred_instances = pred_instances
+        pred_instances = pred_instances.cpu().numpy()
+        detections = sv.Detections(
+            xyxy=pred_instances['bboxes'],
+            class_id=pred_instances['labels'],
+            confidence=pred_instances['scores']
+        )
 
-    pred_instances = pred_instances.cpu().numpy()
-    detections = sv.Detections(
-        xyxy=pred_instances['bboxes'],
-        class_id=pred_instances['labels'],
-        confidence=pred_instances['scores']
-    )
+        txt_labels = [(texts[class_id][0], confidence) for class_id, confidence in zip(detections.class_id, detections.confidence)]
 
-    txt_labels = [(texts[class_id][0], confidence) for class_id, confidence in zip(detections.class_id, detections.confidence)]
+        labels = [
+            f"{class_id} {confidence:0.2f}"
+            for class_id, confidence
+            in zip(detections.class_id, detections.confidence)
+        ]
 
-    labels = [
-        f"{class_id} {confidence:0.2f}"
-        for class_id, confidence
-        in zip(detections.class_id, detections.confidence)
-    ]
+        image = PIL.Image.open(input_image)
+        svimage = np.array(image)
+        svimage = self.bounding_box_annotator.annotate(svimage, detections)
+        svimage = self.label_annotator.annotate(svimage, detections, labels)
+        return (svimage[:, :, ::-1], labels, detections, txt_labels)
 
-    image = PIL.Image.open(input_image)
-    svimage = np.array(image)
-    svimage = bounding_box_annotator.annotate(svimage, detections)
-    svimage = label_annotator.annotate(svimage, detections, labels)
-    return (svimage[:, :, ::-1], labels, detections, txt_labels)
-
-
-# load config
-cfg = Config.fromfile(
-    "./yolo_world_configs_and_weights/yolo_world_l_t2i_bn_2e-4_100e_4x8gpus_obj365v1_goldg_train_lvis_minival.py"
-)
-cfg.work_dir = "."
-cfg.load_from = "./yolo_world_configs_and_weights/yolow-v8_l_clipv2_frozen_t2iv2_bn_o365_goldg_pretrain.pth"
-runner = Runner.from_cfg(cfg)
-runner.call_hook("before_run")
-runner.load_or_resume()
-pipeline = cfg.test_dataloader.dataset.pipeline
-runner.pipeline = Compose(pipeline)
-runner.model.eval()
-
-images = ["1.png", "222.png", "115.png"]
-
-for img_url in images:
-    print("Analysing: ", img_url)
-    (img, labels, detections, txt_labels) = run_image(runner,img_url)
-    #print("labels: ", labels)
-    #print("detections: ", detections)
-    print("txt_labels", txt_labels)
-    with sv.ImageSink(target_dir_path='/mount/test') as sink:
-        sink.save_image(img, "proc_"+img_url)
+    def process_image(self, img_url):
+        print("Analysing: ", img_url)
+        (img, labels, detections, txt_labels) = run_image(runner,img_url)
+        #print("labels: ", labels)
+        #print("detections: ", detections)
+        #print("txt_labels", txt_labels)
+        return txt_labels
+        #with sv.ImageSink(target_dir_path='/mount/test') as sink:
+        #    sink.save_image(img, "proc_"+img_url)
 
 #img = run_image(runner,"1.png")
 #file = open('dog_processed.jpeg', 'w')
@@ -117,3 +111,8 @@ for img_url in images:
 
 #with sv.ImageSink(target_dir_path='/mount/test') as sink:
 #    sink.save_image(img)
+
+if __name__ == "__main__":
+    ywe = YOLOWorldExtractor()
+    labels = ywe.process_image("scene_pics/train_28/115.png")
+    print(labels)

26cf89894adb0790434fca5d2387f1956cb059c0
Arturs Elksnis <archie1983@hotmail.co.uk>
2025-07-09 14:42:47 +0100

    Updated path to the pth model

--- analyze_scene.py
+++ analyze_scene.py
@@ -1,5 +1,5 @@
 from model_navigation import SceneAnalyzer
-sa = SceneAnalyzer("accuracy_093.pth")
+sa = SceneAnalyzer("test_set_contamination/accuracy_093.pth")
 sa.next_best_move("harvested_data/h_7/expl_9/1.png")
 sa.next_best_move("harvested_data/h_7/expl_9/2.png")
 sa.next_best_move("harvested_data/h_7/expl_9/3.png")

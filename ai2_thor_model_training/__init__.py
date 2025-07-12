c027b5feb36e0de3d222bae107abef04b17b484c
Arturs Elksnis <archie1983@hotmail.co.uk>
2025-07-12 23:06:00 +0100

    Continuing to re-structurize for packaging

---
+++ ai2_thor_model_training/__init__.py
@@ -0,0 +1,18 @@
+from .ai2_thor_utils import (AI2THORUtils, is_point_inside_room_ground_truth,
+                            get_rooms_ground_truth,
+                            get_visible_objects_from_collection,
+                            get_all_objects, get_all_objects_of_type,
+                            get_path_length, get_centre_of_the_room,
+                            room_this_point_belongs_to, angle_to_turn_to_face_p2_from_p1,
+                            convert_pose_set2tuple, normalize_colors, action_mapping, inverted_action_mapping,
+                            action_to_index, index_to_action)
+__all__ = ["AI2THORUtils", "is_point_inside_room_ground_truth",
+                            "get_rooms_ground_truth",
+                            "get_visible_objects_from_collection",
+                            "get_all_objects", "get_all_objects_of_type",
+                            "get_path_length", "get_centre_of_the_room",
+                            "room_this_point_belongs_to", "angle_to_turn_to_face_p2_from_p1",
+                            "convert_pose_set2tuple", "normalize_colors", "action_mapping", "inverted_action_mapping",
+                            "action_to_index", "index_to_action"]
+
+__version__ = "1.0.0"

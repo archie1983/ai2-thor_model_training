96f2280208baf5d2c4b1eca37b4245d525dbae85
Arturs Elksnis <archie1983@hotmail.co.uk>
2026-06-17 22:52:03 +0100

    refactor

--- ai2_thor_model_training/model_navigation/__init__.py
+++
@@ -1,4 +0,0 @@
-from .scene_analyzer import SceneAnalyzer
-from .fuzzy_navigation_controller import FuzzyNavigationController
-from .scene_navigator import SceneNavigator
-from .nn_performance_analysis import NNPerformanceAnalyzer
--- ai2_thor_model_training/model_training/__init__.py
+++
@@ -1,7 +0,0 @@
-from .habitat_neural_network import HabitatNeuralNetwork
-from .habitat_nn_utils import (habitat_pics_transform)
-from .habitat_dataset import HabitatDataset
-from .habitat_data_loading import HabitatDataLoading
-from .habitat_nn_utils import load_model_architecture
-from .habitat_nn_trainer import HabitatNNTrainer
-from .hyper_params import HyperParameters
